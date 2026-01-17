4f97d7073dfedcb6272009b600c01c8abd4aaadc
bittusing <abhilekhk6@gmail.com>
2026-01-17 18:17:16 +0530

    Comment out credentials option in CORS middleware for deployment consistency

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ connectDB();
 app.use(cors({
 //   origin: process.env.FRONTEND_URL || 'http://localhost:3000' || 'https://flywell-logistics.vercel.app',
   origin: 'https://flywell-logistics.vercel.app/',
-  credentials: true
+//   credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
