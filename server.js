c61376417654b64d11d869a8770f2817c0f79c99
bittusing <abhilekhk6@gmail.com>
2026-03-21 23:59:50 +0530

    uplode

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.use(cors({
     'https://admin.flywelllogistics.com',
     'https://www.flywelllogistics.com',
   ]
-}))
+}))  
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
